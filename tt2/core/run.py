3255603c3e2534111af56922bec3168f03891715
Brett Currie <theycallmevotum@gmail.com>
2019-02-02 15:55:37 -0400

    #11 - Include argparser in run.py.
    - Remove unneccesary __main__ entry point in parse_artifacts.
    - Allowing for both entry points here. -start / -parse_artifacts

--- tt2/core/run.py
+++ tt2/core/run.py
@@ -4,11 +4,30 @@ run.py
 Main entry point into bot script. Ensuring that the directory is
 manually added to the system path so all modules are able to be imported.
 """
+import argparse
+
 from os import sys, getcwd
 sys.path.append("\\".join(getcwd().split("\\")[:-2]))
 
 if __name__ == '__main__':
-    from tt2.core.bot import Bot
+    # Begin argument setup/build.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-start", action="store_true", default=False, help="Initialize a new bot instance and begin automation. "
+                                                           "(Ensure that the bot screen is open and at the top-left "
+                                                           "most portion of the screen).")
+    parser.add_argument(
+        "-parse_artifacts", action="store_true", default=False, help="Parse artifacts in game. "
+                                                                     "May take up to two minutes. Check your stats "
+                                                                     "file afterwards to view the results.")
 
-    # Initialize bot directly here and begin game loop.
-    Bot().run()
+    arguments = parser.parse_args()
+    if arguments.start:
+        from tt2.core.bot import Bot
+        Bot().run()
+
+    elif arguments.parse_artifacts:
+        from tt2.tools.parse_artifacts import parse_artifacts
+        parse_artifacts()
+    else:
+        parser.print_help()
--- tt2/tools/parse_artifacts.py
+++ tt2/tools/parse_artifacts.py
@@ -5,6 +5,18 @@ Make use of this file to manually start a check to determine what artifacts are 
 each one. Can be useful for keeping track of progress in the game. This also takes care of modifying the logging
 used by the bot to place logs during the parsing into a different log file for tracking artifact parsing logs.
 """
+from settings import TOOL_LOG_DIR, CONFIG_FILE
+from tt2.core.bot import Bot
+from tt2.core.utilities import make_logger
+
+import datetime
+
+LOGGER_FILE_NAME_STRFMT = "%Y-%m-%d_%H-%M-%S"
+INIT_DATE_FMT = datetime.datetime.strftime(datetime.datetime.now(), LOGGER_FILE_NAME_STRFMT)
+LOGGER_FILE_NAME = "{log_dir}/{file}-{name}.log".format(
+    file="parse_artifacts", log_dir=TOOL_LOG_DIR, name=INIT_DATE_FMT
+)
+logger = make_logger(log_file=LOGGER_FILE_NAME)
 
 
 def parse_artifacts():
@@ -21,18 +33,3 @@ def parse_artifacts():
 
     bot.stats.write()
     logger.info("artifacts have been parsed successfully, check your stats file for output")
-
-
-if __name__ == '__main__':
-    from settings import TOOL_LOG_DIR, CONFIG_FILE
-    from tt2.core.bot import Bot
-    from tt2.core.utilities import make_logger
-
-    import datetime
-
-    LOGGER_FILE_NAME_STRFMT = "%Y-%m-%d_%H-%M-%S"
-    INIT_DATE_FMT = datetime.datetime.strftime(datetime.datetime.now(), LOGGER_FILE_NAME_STRFMT)
-    LOGGER_FILE_NAME = "{log_dir}/{file}-{name}.log".format(file="parse_artifacts", log_dir=TOOL_LOG_DIR, name=INIT_DATE_FMT)
-
-    logger = make_logger(log_file=LOGGER_FILE_NAME)
-    parse_artifacts()
