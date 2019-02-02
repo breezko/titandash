0afd2841b552925b2bb0aad647109e3c29c23389
Brett Currie <theycallmevotum@gmail.com>
2019-02-02 14:58:02 -0400

    Update run.py to allow for importing any module in repository.

--- tt2/core/run.py
+++ tt2/core/run.py
@@ -1,10 +1,11 @@
 """
 run.py
 
-Main entry point into bot script.
+Main entry point into bot script. Ensuring that the directory is
+manually added to the system path so all modules are able to be imported.
 """
-from os import sys, path
-sys.path.append(path.dirname(path.dirname(path.abspath("tt2"))))
+from os import sys, getcwd
+sys.path.append("\\".join(getcwd().split("\\")[:-2]))
 
 if __name__ == '__main__':
     from tt2.core.bot import Bot
