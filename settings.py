00cb9ff197c68b8bb99fdd330dafbbc5b9bb665e
Brett Currie <theycallmevotum@gmail.com>
2019-08-16 08:15:43 -0300

    #128 Update bot version to newest (1.3.0)

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ Store all project specific settings here.
 import os
 import git
 
-BOT_VERSION = "1.2.0"
+BOT_VERSION = "1.3.0"
 
 # Store the root directory of the project. May be used and appended to files in other directories without
 # the need for relative urls being generated to travel to the file.
