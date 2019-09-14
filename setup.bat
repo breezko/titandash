8c1172cdc6284f78a99dec932f247ef68bf471ce
Brett Currie <theycallmevotum@gmail.com>
2019-09-14 11:43:00 -0300

    #148 Create system tray application for titandash functionality.
    - Click icon to open dashboard.
    - Created management command for sending offline signal to backend auth.
    - Migrations/static files/db location/start and stop server are all available.
    - Batch files available for early setup as well as starting system tray app.

---
+++ setup.bat
@@ -0,0 +1,15 @@
+:: Create Virtualenv.
+:: Expecting virtualenvwrapper-win
+call mkvirtualenv titandash -r requirements.txt
+
+:: Using new virtual environment, make and migrate.
+call %WORKON_HOME%/titandash/Scripts/python titanbot/manage.py makemigrations
+call %WORKON_HOME%/titandash/Scripts/python titanbot/manage.py migrate
+
+:: Install node modules.
+call npm install
+
+:: Collect static files.
+call %WORKON_HOME%/Scripts/python titanbot/manage.py collectstatic --noinput
+
+exit
