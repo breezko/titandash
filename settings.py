30809666ce6e44a9bd78ebc7e0d1bbcf0464e3f2
Brett Currie <theycallmevotum@gmail.com>
2019-10-03 11:29:34 -0300

    #206 Update and improve project settings.
    - Using a new version.json file, storing our version information in a single external location.
    - Removed some logging and bloat from the database mover for old versions.
    - Added local data directories for auto updater, backups, logs.
    - Including our scripts in the settings for use later on if needed.
    - Titandash loader/dashboard urls in settings.
    - Setting the port used explicitly here.

--- settings.py
+++ settings.py
@@ -4,21 +4,29 @@ settings.py
 Store all project specific settings here.
 """
 import os
-import logging
 import pathlib
-import textwrap
-
-logger = logging.getLogger(__name__)
+import json
 
 
-BOT_VERSION = "1.5.4.01"
+# Version file used to determine the current project version.
+VERSION_FILE = "version.json"
 
-
-# Grab the windows local users directory (ie: C:/Users/<username>).
-USER_DIR = str(pathlib.Path.home())
 # Store the root directory of the project. May be used and appended to files in other directories without
 # the need for relative urls being generated to travel to the file.
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(ROOT_DIR, VERSION_FILE), "r") as version_file:
+    BOT_VERSION = json.load(version_file)["version"]
+
+# Specify the port that the django server will listen on.
+TITANDASH_PORT = 8000
+
+# Specify the url used by the application to derive accessible status.
+TITANDASH_DASHBOARD_URL = "http://localhost:%d/" % TITANDASH_PORT
+TITANDASH_LOADER_URL = "http://localhost:%d/bootstrap" % TITANDASH_PORT
+
+# Grab the windows local users directory (ie: C:/Users/<username>).
+USER_DIR = str(pathlib.Path.home())
 # Django project directory (titanbot).
 PROJECT_DIR = os.path.join(ROOT_DIR, "titanbot")
 # TitanDash project directory (titandash).
@@ -36,11 +44,21 @@ DATA_DIR = os.path.join(BOT_DIR, "data")
 # Additional data directories.
 IMAGE_DIR = os.path.join(DATA_DIR, "images")
 
+# Scripts.
+PROGRAM_BAT = os.path.join(ROOT_DIR, "titandash.bat")
+PROGRAM_SETUP_BAT = os.path.join(ROOT_DIR, "setup.bat")
+
 # Additional data directories used to store local data
 # on the users machine.
 LOCAL_DATA_DIR = os.path.join(USER_DIR, ".titandash")
 # Directory to store our database in.
 LOCAL_DATA_DB_DIR = os.path.join(LOCAL_DATA_DIR, "db")
+# Directory to store newly downloaded version in.
+LOCAL_DATA_UPDATE_DIR = os.path.join(LOCAL_DATA_DIR, "update")
+# Directory to store the latest backup in.
+LOCAL_DATA_BACKUP_DIR = os.path.join(LOCAL_DATA_DIR, "backup")
+# Directory to store our titandash logs in.
+LOCAL_DATA_LOG_DIR = os.path.join(LOCAL_DATA_DIR, "logs")
 
 # Testing directory.
 TEST_DIR = os.path.join(TITANDASH_DIR, "tests")
@@ -53,7 +71,6 @@ THEMES_DIR = os.path.join(TITANDASH_DIR, "static/css/theme")
 # Make sure a "logs" directory actually exists.
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
-
 
 TITAN_DB_NAME = "titan.sqlite3"
 TITAN_DB_PATH = os.path.join(LOCAL_DATA_DB_DIR, TITAN_DB_NAME)
@@ -89,40 +106,22 @@ def user_directory():
         os.makedirs(LOCAL_DATA_DIR)
     if not os.path.exists(LOCAL_DATA_DB_DIR):
         os.makedirs(LOCAL_DATA_DB_DIR)
+    if not os.path.exists(LOCAL_DATA_UPDATE_DIR):
+        os.makedirs(LOCAL_DATA_UPDATE_DIR)
+    if not os.path.exists(LOCAL_DATA_BACKUP_DIR):
+        os.makedirs(LOCAL_DATA_BACKUP_DIR)
+    if not os.path.exists(LOCAL_DATA_LOG_DIR):
+        os.makedirs(LOCAL_DATA_LOG_DIR)
 
     # Our local data directory is guaranteed to exist now, check for the existence of our actual
     # database within the codebase, and move it if one exists.
     existing = [p for p in pathlib.Path(ROOT_DIR).glob("**/{titan_db}".format(titan_db=TITAN_DB_NAME))]
 
-    # Spit out a helpful warning if a user has more than one database in their source directory.
-    # This isn't common, but would happen potentially.
-    if len(existing) > 1:
-        logger.warning(textwrap.dedent("""\
-            It looks like you have more than one database present in your titandash project.
-                
-            The first database found will be moved into your local data directory ({local_dir}), but you can manually
-            place a database at that location if you want to use a different one.
-            
-            Titandash uses the following path when connecting to your database by default: {local}.
-        """.format(
-            local_dir=LOCAL_DATA_DB_DIR,
-            local=TITAN_DB_PATH
-        )))
-        logger.warning(
-            "Databases found:\n{dbs}".format(
-                dbs="\n".join([str(p) for p in existing])
-            ))
-
     # Renaming and moving the database present into the local database directory.
     # If a database is already present, we can ignore this part so no overwrite of
     # the users database takes place.
     if existing:
-        if not os.path.exists(TITAN_DB_PATH):
-            existing[0].rename(TITAN_DB_PATH)
-            logger.info("Database: {local} has been moved to {database}".format(
-                local=str(existing[0]),
-                database=TITAN_DB_PATH
-            ))
+        existing[0].rename(TITAN_DB_PATH)
 
 
 user_directory()
