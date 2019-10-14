24d8aab45f042ceb9815a1fc32c4451cb7ec1a53
Brett Currie <theycallmevotum@gmail.com>
2019-10-14 09:01:57 -0300

    #275 Fix unquoted batch commands.
    - All batch commands are now wrapped in quotations to support paths with
    spaces.

--- titandash.bat
+++ titandash.bat
@@ -1,4 +1,4 @@
 @echo off
 call workon titandash
-call cd %~dp0%titanbot
-call %WORKON_HOME%/titandash/Scripts/python titandash.py
+call cd "%~dp0%titanbot"
+call "%WORKON_HOME%/titandash/Scripts/python" titandash.py
