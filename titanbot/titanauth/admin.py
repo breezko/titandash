c871d57433ad4cb5405478dbf95ca1e4b60ef296
Brett Currie <theycallmevotum@gmail.com>
2019-10-17 15:40:28 -0300

    #278 Include release info model in titanauth admin.

--- titanbot/titanauth/admin.py
+++ titanbot/titanauth/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 
-# Register your models here.
+from titanauth.models.release_info import ReleaseInfo
+
+
+@register(ReleaseInfo)
+class ReleaseInfoAdmin(admin.ModelAdmin):
+    list_display = ["version", "grabbed"]
