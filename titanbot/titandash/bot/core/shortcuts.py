7417832fbe9c91f8eed66989f8379a8d8c8d4bf7
Brett Currie <theycallmevotum@gmail.com>
2019-10-29 13:59:41 -0300

    #320 Update shortcuts module to use global implementation.
    - Using a global implementation here to allow for multiple loggers/instances used in our base callbacks.
    - A special _HOOKED flag is also present which lets us hook the keyboard module once and only once.
    - Callbacks now loop through available instances/loggers so that regardless of the instances running, shortcuts queue functions for each one.

--- titanbot/titandash/bot/core/shortcuts.py
+++ titanbot/titandash/bot/core/shortcuts.py
@@ -10,6 +10,7 @@ from django.utils import timezone
 from titandash.models.queue import Queue
 from titandash.bot.core.constants import FUNCTION_SHORTCUTS, SHORTCUT_FUNCTIONS
 
+import keyboard
 import operator
 import collections
 import datetime
@@ -19,66 +20,154 @@ SHIFT = "shift"
 CTRL = "ctrl"
 ALT = "alt"
 
+_HOOKED = False
+LOGGERS = []
+INSTANCES = []
+COOLDOWN = 2
 
-class ShortcutListener:
-    def __init__(self, logger, instance, cooldown=2):
-        self.logger = logger
-        self.instance = instance
-        self.cooldown = cooldown
+TIMESTAMP = timezone.now()
+RESUME = timezone.now()
 
-        # Using timestamp and resume variables to block after a shortcut
-        # is pressed so the same function isn't queued up twice.
-        self.timestamp = timezone.now()
-        self.resume = timezone.now()
+CURRENT = dict()
+INDEX = 0
 
-        # Current is used to determine active modifiers/key presses.
-        self.current = dict()
-        self.index = 0
 
-    @staticmethod
-    def _fix_event(event):
-        """
-        Fix the event so that any shift, ctrl or alt buttons can be used to initiate a combo.
-        """
-        split = event.name.split(" ")[-1]
-        if split in [SHIFT, CTRL, ALT]:
-            return split
+def add_handle(instance, logger):
+    """
+    Add a new handler to our shortcuts module.
 
-        return event.name
+    Each handler is added to our globally available data sets, and are used when a shortcut
+    is activated to send out a log message, as well as queue up the function specified.
+    """
+    global INSTANCES, LOGGERS
 
-    def on_press(self, event):
-        """
-        When a keypress takes place, some checks take place to fixup the event passed and additional checks
-        are present to allow a user to use keyboard combinations to activate functionality within a Bot.
-        """
-        self.timestamp = timezone.now()
+    INSTANCES.append(instance)
+    LOGGERS.append(logger)
+    logger.info("{instance} added to shortcut module.".format(instance=instance))
 
-        if self.index < 0:
-            self.index = 0
-        if event:
-            key = self._fix_event(event=event)
-            if key in self.current:
-                return
 
-            # Determine if the key pressed is present in any of the available shortcuts.
-            for short in FUNCTION_SHORTCUTS.keys():
-                if key in short.split("+"):
-                    self.current[key] = self.index
-                    self.index += 1
-                    break
+def clear_handles():
+    """
+    Clear all handlers currently present.
+    """
+    global INSTANCES, LOGGERS
 
-            if len(self.current) > 0:
-                combo = "+".join(collections.OrderedDict(sorted(self.current.items(), key=operator.itemgetter(1))).keys())
-                if combo in FUNCTION_SHORTCUTS:
-                    if self.timestamp > self.resume:
-                        Queue.objects.add(function=FUNCTION_SHORTCUTS[combo], instance=self.instance)
-                        self.logger.debug("{combo} pressed, executing '{func}'".format(combo=combo, func=SHORTCUT_FUNCTIONS[FUNCTION_SHORTCUTS[combo]]))
-                        self.resume = self.resume + datetime.timedelta(seconds=1)
+    INSTANCES.clear()
+    LOGGERS.clear()
 
-    def on_release(self, event):
-        """When a keypress is released, removing that key from the current set of active keys."""
-        try:
-            self.current.pop(self._fix_event(event=event))
-            self.index -= 1
-        except KeyError:
-            pass
+
+def hook():
+    """
+    Hook the keyboard modules on_press and on_release functionality with our custom
+    shortcuts functionality.
+
+    We only ever hook once, and our loggers/instances present are actually used when
+    a callback is executed.
+    """
+    global _HOOKED
+
+    if not _HOOKED:
+        keyboard.on_press(callback=on_press)
+        keyboard.on_release(callback=on_release)
+
+        # Flipping our flag now, hook will be called again for each instance
+        # being started, but only one hook call is ever successfully called.
+        _HOOKED = True
+
+
+def unhook(instance, logger):
+    """
+    Unhook the specified instance from the shortcuts module.
+
+    This is done when a session is terminated through some means available to the user, or an error occurs.
+
+    When this happens, we may have other instances still running, in which case, we want to only remove
+    the specified instance from our logger/instances list.
+    """
+    global INSTANCES, LOGGERS
+
+    for index, _instance in enumerate(INSTANCES):
+        if instance == _instance:
+            del INSTANCES[index]
+            break
+    for index, _logger in enumerate(LOGGERS):
+        if logger == _logger:
+            del LOGGERS[index]
+            break
+
+
+def _fix_event(event):
+    """
+    Fix the event so that any shift, ctrl or alt buttons can be used to initiate a combo.
+    """
+    split = event.name.split(" ")[-1]
+    if split in [SHIFT, CTRL, ALT]:
+        return split
+
+    return event.name
+
+
+def _log(message, level="info"):
+    """
+    Generate a log message for each available module logger present.
+    """
+    global LOGGERS
+
+    # Looping through each logger available, sending the specified message to them.
+    # We do this so we can have multiple instances using the same keyboard events.
+    for logger in LOGGERS:
+        getattr(logger, level)(message)
+
+
+def _queue(function):
+    """
+    Generate a queue object for the specified function for each available instance.
+    """
+    global INSTANCES
+
+    # Looping through each instance available, generating a queue object for
+    # all of them. Ensuring that multiple instances generate the same functions.
+    for instance in INSTANCES:
+        Queue.objects.create(function=function, instance=instance)
+
+
+def on_press(event):
+    """
+    When a keypress takes place, some checks take place to fixup the event passed and additional checks
+    are present to allow a user to use keyboard combinations to activate functionality within a Bot.
+    """
+    global TIMESTAMP, INDEX, RESUME, INSTANCES
+
+    TIMESTAMP = timezone.now()
+    if INDEX < 0:
+        INDEX = 0
+    if event:
+        key = _fix_event(event=event)
+        if key in CURRENT:
+            return
+
+        # Determine if the key pressed is present in any of the available shortcuts.
+        for short in FUNCTION_SHORTCUTS.keys():
+            if key in short.split("+"):
+                CURRENT[key] = INDEX
+                INDEX += 1
+                break
+
+        if len(CURRENT) > 0:
+            combo = "+".join(collections.OrderedDict(sorted(CURRENT.items(), key=operator.itemgetter(1))).keys())
+            if combo in FUNCTION_SHORTCUTS:
+                if TIMESTAMP > RESUME:
+                    _queue(function=FUNCTION_SHORTCUTS[combo])
+                    _log(message="{combo} pressed, adding '{func}' to queued functions...".format(combo=combo, func=FUNCTION_SHORTCUTS[combo]))
+                    RESUME = RESUME + datetime.timedelta(seconds=1)
+
+
+def on_release(event):
+    """When a keypress is released, removing that key from the current set of active keys."""
+    global CURRENT, INDEX
+
+    try:
+        CURRENT.pop(_fix_event(event=event))
+        INDEX -= 1
+    except KeyError:
+        pass
