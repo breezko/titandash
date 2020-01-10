bc530600e42414194fe982c2ff86d558b65a98fb
Votum <theycallmevotum@gmail.com>
2020-01-10 15:12:29 -0400

    #434 Update globals to support logging settings.

--- titanbot/titandash/models/globals.py
+++ titanbot/titandash/models/globals.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from titandash.constants import LOGGING_LEVEL_CHOICES, INFO
+
 
 HELP_TEXT = {
     "failsafe_settings": "Enable or disable the failsafe functionality when a bot session is running. Note that turning this setting off may make it difficult to kill a bot session if an unrecoverable failure occurs. You can always force quit the application if needed though.",
     "event_settings": "Enable or disable the different functionality depending on whether or not an event is currently running in game that modifies in game locations.",
-    "pihole_ads_settings": "Enable or disable the ability to watch and collect ads without vip while using pihole to prevent ads from running within tap titans 2. This allows users to basically get the benefits of VIP, without a VIP enabled account."
+    "pihole_ads_settings": "Enable or disable the ability to watch and collect ads without vip while using pihole to prevent ads from running within tap titans 2. This allows users to basically get the benefits of VIP, without a VIP enabled account.",
+    "logging_level": "Choose a logging level that will be used by bot sessions when they are running.",
 }
 
 
@@ -58,6 +61,7 @@ class GlobalSettings(models.Model):
     failsafe_settings = models.CharField(verbose_name="Failsafe Settings", max_length=255, choices=FAILSAFE_CHOICES, default=ON, help_text=HELP_TEXT["failsafe_settings"])
     event_settings = models.CharField(verbose_name="In Game Event Settings", max_length=255, choices=EVENT_CHOICES, default=OFF, help_text=HELP_TEXT["event_settings"])
     pihole_ads_settings = models.CharField(verbose_name="Enable PI-Hole Ads", max_length=255, choices=PIHOLE_ADS_CHOICES, default=OFF, help_text=HELP_TEXT["pihole_ads_settings"])
+    logging_level = models.CharField(verbose_name="Logging Level", max_length=255, choices=LOGGING_LEVEL_CHOICES, default=INFO, help_text=HELP_TEXT["logging_level"])
 
     def __str__(self):
         return "GlobalSettings {id}".format(id=self.pk)
@@ -70,7 +74,8 @@ class GlobalSettings(models.Model):
             "id": self.pk,
             "failsafe_settings": self.failsafe_settings,
             "event_settings": self.event_settings,
-            "pihole_settings": self.pihole_ads_settings
+            "pihole_settings": self.pihole_ads_settings,
+            "logging_level": self.logging_level,
         }
 
     def form_dict(self):
@@ -84,6 +89,7 @@ class GlobalSettings(models.Model):
                 "failsafe_settings": FAILSAFE_CHOICES,
                 "event_settings": EVENT_CHOICES,
                 "pihole_settings": PIHOLE_ADS_CHOICES,
+                "logging_level": LOGGING_LEVEL_CHOICES
             }
         }
 
